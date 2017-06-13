df9d3f50377f489b12e41b44dd7cf63feaa779e8
Jonathan Hemnes <jonathan.hemnes@talentkode.com>
2017-06-12 20:37:03 -0600

    moved to promises

--- tweetRetreiver.js
+++ tweetRetreiver.js
@@ -11,40 +11,46 @@ const client = new Twitter({
 
 const getReplacedTweets = function () {
     return client.get('statuses/user_timeline', { screen_name: 'cnn' }).then(results => {
-        const donaldTrumpTweets = getDonaldTrumpRelatedTweets(results);
-        const replacedTweets = replaceTrumpWithClinton(donaldTrumpTweets);
-        const cleanedTweets = removeUrlsFromTweets(replacedTweets);
-        return cleanedTweets
+        return getDonaldTrumpRelatedTweets(results)
+            .then(replaceTrumpWithClinton)
+            .then(removeUrlsFromTweets);
     })
+
 }
 
 const removeUrlsFromTweets = function (tweets) {
-    return tweets.map(tweet => {
-        tweet.text = tweet.text.replace(/https:\/\/t.co\/.+\b/g, '').replace('…', '')
-        return tweet;
+    return new Promise((resolve, reject) => {
+        resolve(tweets.map(tweet => {
+            tweet.text = tweet.text.replace(/https:\/\/t.co\/.+\b/g, '').replace('…', '')
+            return tweet;
+        }))
     })
 }
 
 
 const getDonaldTrumpRelatedTweets = function (tweets) {
-    return tweets.filter(result => {
-        const parsedText = result.text.toUpperCase();
-        return parsedText.includes('TRUMP') || parsedText.includes('DONALD TRUMP');
+    return new Promise((resolve, reject) => {
+        resolve(tweets.filter(result => {
+            const parsedText = result.text.toUpperCase();
+            return parsedText.includes('TRUMP') || parsedText.includes('DONALD TRUMP');
+        }))
     })
 }
 
 const replaceTrumpWithClinton = function (tweets) {
-    return tweets.map(tweet => {
-        if (tweet.text.includes('Donald Trump')) {
-            tweet.text = tweet.text.replace('Donald Trump', 'Hillary Clinton');
-        }
-        if (tweet.text.includes('Melania')) {
-            tweet.text = tweet.text.replace('Melania', 'Bill');
-        }
-        if (tweet.text.includes('Trump')) {
-            tweet.text = tweet.text.replace('Trump', 'Clinton');
-        }
-        return tweet;
+    return new Promise((resolve, reject) => {
+        resolve(tweets.map(tweet => {
+            if (tweet.text.includes('Donald Trump')) {
+                tweet.text = tweet.text.replace('Donald Trump', 'Hillary Clinton');
+            }
+            if (tweet.text.includes('Melania')) {
+                tweet.text = tweet.text.replace('Melania', 'Bill');
+            }
+            if (tweet.text.includes('Trump')) {
+                tweet.text = tweet.text.replace('Trump', 'Clinton');
+            }
+            return tweet;
+        }))
     })
 }
 
