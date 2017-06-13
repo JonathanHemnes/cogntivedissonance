f5271ec36a2f51cc71b8b5fef4e3fc027bfd35e9
Jonathan Hemnes <jonathan.hemnes@talentkode.com>
2017-06-12 22:23:28 -0600

    starting to do things now

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
 const tweetRetreiver = require('./tweetRetreiver');
 const sources = require('./sources');
 const Promise = require('bluebird');
+var Database = require('better-sqlite3');
+var db = new Database('database.sqlite', {});
+
+
+const createSourceIfNotExists = function(screenName) {
+    let rows = db.prepare('INSERT INTO Tweet(ScreenName) SELECT ? WHERE NOT EXISTS(SELECT 1 FROM Tweet WHERE screenName = ?);').run(screenName, screenName)
+    console.log(rows);
+}
 
 sources.forEach(source => {
+    createSourceIfNotExists(source);
     tweetRetreiver(source).then(tweets => {
         console.log(tweets);
     })
 })
+
+
+
+
 
 
 
